b4d8b44717d86f82d2bb576e97c08ea33cdfce76
Azimuddin1729 <azimuddin3006@gmail.com>
2024-02-20 09:08:26 +0530

    First Commit

---
+++ text.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+using namespace std;
+#define int long long  
+#define po pop_back
+#define pb push_back
+#define vi vector<int>
+#define vp vector<pair<int,int>>
+#define vvi vector<vi>
+#define out(x) cout<<x<<endl
+#define ri(x) cin>>x
+#define rv(n,v) for(int i=0;i<n;i++){ cin>>v[i];}
+#define rv1(n,v) for(int i=1;i<=n;i++){  cin>> v[i];}
+#define loop(i,t) for(int i=0;i<t;i++)
+#define loopr(i,t) for(int i=t-1;i>=0;i--)
+#define lb lower_bound
+#define ub upper_bound
+#define endl '\n'
+#define all(v) v.begin(),v.end()
+ 
+signed main(){ 
+ cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    cout<<"Hello and welcome to GIT"<<endl;
+}
